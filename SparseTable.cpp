f5b124165b05881bdf543e629d037f106f79c826
Mayank <mayank.bharthwal@indiamart.com>
2019-04-18 15:54:05 +0530

    started sparse tree

---
+++ SparseTable.cpp
@@ -0,0 +1,60 @@
+/*
+ * author: bmayank
+ * URL : #{problem_url}
+ */
+
+#include <bits/stdc++.h>
+
+//optimization of program
+#define eb emplace_back
+#define gc getchar_unlocked
+//end of optmization
+
+//shorten coding time
+#define ll long long
+#define ui unsigned int
+#define ull unsigned long long
+#define power9 (int)(1e9 + 7)
+#define totalChars 25
+#define Pi pair<int, int>
+#define vi vector<int>
+#define vll vector<vll>
+#define vull vector<ull>
+//end of shorten coding time
+
+//helper code
+#define forEach(c) for (auto &value : c)
+#define loopab(loopconstant, startingValue, endingValue, adder) for (loopconstant = (startingValue); loopconstant <= (endingValue); loopconstant += adder)
+#define loop(loopconstant1, stopper) loopab(loopconstant1, 0, stopper, 1)
+
+#define Odd(n) if (n & 1)
+#define cases \
+	ull t;      \
+	cin >> t;   \
+	while (t--)
+//end of helper code
+
+//debugging code
+#define watch(x) cout << (#x) << " is " << (x) << "\n"
+
+const char lineEnd = '\n';
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	try
+	{
+		//write your code here
+	}
+	catch (exception const &e)
+	{
+		cout << "There was an error: " << e.what() << lineEnd;
+	}
+
+	return 0;
+}
