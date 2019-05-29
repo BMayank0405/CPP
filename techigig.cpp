a5d373eb820c56c46260467a92f937e8d0d493ed
Mayank <mayank.bharthwal@indiamart.com>
2019-05-29 16:22:54 +0530

    techgig code

--- techigig.cpp
+++ techigig.cpp
@@ -17,9 +17,11 @@
 #define power9 (int)(1e9 + 7)
 #define totalChars 25
 #define Pi pair<int, int>
-#define vi vector<int>
-#define vll vector<ll>
-#define vull vector<ull>
+#define vec vector
+#define vi vec<int>
+#define vll vec<ll>
+#define vull vec<ull>
+#define endl "\n";
 //end of shorten coding time
 
 //helper code
@@ -29,15 +31,13 @@
 
 #define Odd(n) if (n & 1)
 #define cases \
-	short t;    \
+	ull t;      \
 	cin >> t;   \
 	while (t--)
 //end of helper code
 
 //debugging code
 #define watch(x) cout << (#x) << " is " << (x) << "\n"
-
-const char lineEnd = '\n';
 
 using namespace std;
 
@@ -51,76 +51,45 @@ int main()
 	{
 		cases
 		{
-			ui size, i;
-			cin >> size;
-			vector<ll> elements(size);
-			vector<ll> sum(size);
-			vector<string> arrOfEl(size);
-			loop(i, size - 1)
+			int boolarr[100001] = {false};
+			ll sum = 0;
+			int N, i;
+			cin >> N;
+			vi arr(N);
+			loop(i, N - 1)
 			{
-				cin >> elements[i];
+				cin >> arr[i];
 			}
+			sort(arr.begin(), arr.end());
 
-			for (int x = size - 1; x >= 0; --x)
+			for (int j = N - 1; j >= 0; j--)
 			{
-				ll rightSum = 0;
-				string rightSubString = "";
-				if (elements[x] > 0)
+				string s = to_string(arr[j]);
+				bool flag = true;
+				for (int z = 0; z < s.length(); z++)
 				{
-
-					if (x + 1 < size - 1)
+					if (boolarr[s[z]])
 					{
-						ll max = -1000000, index = -1;
-						for (ui j = x + 2; j < size; j++)
-						{
-							if (sum[j] >= max)
-							{
-								max = sum[j];
-								index = j;
-							}
-						}
-						//now pick the rightsub array from index
-						if (index > -1)
-						{
-							rightSubString = arrOfEl[index] + to_string(elements[x]);
-							rightSum = max;
-						}
-						else
-						{
-							rightSubString = to_string(elements[x]);
-						}
-					}
-					else
-					{
-						rightSubString = to_string(elements[x]);
+						flag = false;
+						break;
 					}
 				}
-				else
+				if (flag)
 				{
-					rightSubString = to_string(elements[x]);
-				}
-				sum[x] = elements[x] + rightSum;
+					for (int z = 0; z < s.length(); z++)
+					{
+						boolarr[s[z]] = true;
+					}
 
-				//correct here
-				arrOfEl[x] = rightSubString;
-			}
-
-			ll max = -100000, index = -1;
-			for (ui i = 0; i < size; i++)
-			{
-				if (sum[i] > max)
-				{
-					max = sum[i];
-					index = i;
+					sum += arr[j];
 				}
 			}
-			cout << arrOfEl[index];
-			cout << lineEnd;
+			cout << sum << endl;
 		}
 	}
 	catch (exception const &e)
 	{
-		cout << "There was an error: " << e.what() << lineEnd;
+		cout << "There was an error: " << e.what() << endl;
 	}
 
 	return 0;
