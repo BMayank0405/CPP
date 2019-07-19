5f3d79733eb75360e8e2612b512f4b197d8d98da
Mayank <mayank.bharthwal@indiamart.com>
2019-07-19 17:58:57 +0530

    test

---
+++ greedy/motu&patlu.cpp
@@ -0,0 +1,83 @@
+/*
+ * author: bmayank
+ * URL : #{problem_url}
+ */
+
+#include <bits/stdc++.h>
+
+//optimization of program
+#define eb emplace_back
+#define gc getchar_unlocked
+//end of optmization
+
+//shorten coding time
+#define ll long long
+#define ui unsigned int
+#define ull unsigned long long
+#define power9 (int)(1e9 + 7)
+#define totalChars 25
+#define Pi pair<int, int>
+#define vec vector
+#define vi vec<int>
+#define vll vec<ll>
+#define vull vec<ull>
+#define endl "\n";
+//end of shorten coding time
+
+//helper code
+#define forEach(c) for (auto &value : c)
+#define loopab(loopconstant, startingValue, endingValue, adder) for (loopconstant = (startingValue); loopconstant <= (endingValue); loopconstant += adder)
+#define loop(loopconstant1, stopper) loopab(loopconstant1, 0, stopper, 1)
+
+#define Odd(n) if (n & 1)
+#define cases \
+	ull t;      \
+	cin >> t;   \
+	while (t--)
+//end of helper code
+
+//debugging code
+#define watch(x) cout << (#x) << " is " << (x) << "\n"
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	try
+	{
+		cases
+		{
+			int n, i, m;
+			cin >> n;
+			m = (n / 2);
+			int arr[n];
+			double start = 0, end = 0;
+			loop(i, n - 1)
+			{
+				cin >> arr[i];
+				if (i < m)
+				{
+					start += arr[i];
+				}
+				else
+				{
+					if (i > m)
+					{
+						end += arr[i];
+					}
+				}
+			}
+			start /= 2;
+		}
+	}
+	catch (exception const &e)
+	{
+		cout << "There was an error: " << e.what() << endl;
+	}
+
+	return 0;
+}
