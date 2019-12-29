4208eaa60c447038eb576fe17adf3f8c746ab8a0
mayank <mayank.barthwal@xeno.in>
2019-12-29 19:15:10 +0530

    directed graph question

--- graphs/studious-amit-and-his-new-college.cpp
+++ graphs/studious-amit-and-his-new-college.cpp
@@ -1,6 +1,6 @@
 /*
  * author: bmayank
- * URL : PROBLEM_URL
+ * URL : https://www.hackerearth.com/practice/algorithms/graphs/depth-first-search/practice-problems/algorithm/studious-amit-and-his-new-college/
  */
 
 #include <bits/stdc++.h>
@@ -49,6 +49,42 @@
 
 using namespace std;
 
+void addEdge(vec<vui> &graph, ui u, ui v)
+{
+    graph[u].eb(v);
+}
+
+bool hasCycle(vec<vui> &graph, vui &vis, ui s, vui &recStack)
+{
+    vis[s] = 1;
+    recStack[s] = 1;
+    for (int i = 0; i < graph[s].size(); i++)
+    {
+        if (vis[graph[s][i]] == 0 && hasCycle(graph, vis, graph[s][i], recStack))
+            return true;
+        else if (recStack[graph[s][i]])
+            return true;
+    }
+    recStack[s] = 0;
+    return false;
+}
+
+bool isCyclic(vec<vui> &graph, ui n)
+{
+    ui i;
+    vui vis(n, 0);
+    vui recStack(n, 0);
+    bool cycle = false;
+    loop(i, n - 1)
+    {
+        if (vis[i] == 0 && hasCycle(graph, vis, i, recStack))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main()
 {
     ios_base::sync_with_stdio(0);
@@ -58,6 +94,36 @@ int main()
     try
     {
         //write your code here
+
+        cases
+        {
+            ui n, m, i;
+            cin >> n >> m;
+            vec<vui> graph(n, vui());
+            loop(i, m - 1)
+            {
+                ui u, v;
+                cin >> u >> v;
+                --u, --v;
+                addEdge(graph, u, v);
+            }
+            if (m == 0)
+            {
+                cout << 1 << endl;
+            }
+            else
+            {
+
+                if (isCyclic(graph, n))
+                {
+                    cout << 0 << endl;
+                }
+                else
+                {
+                    cout << 1 << endl;
+                }
+            }
+        }
     }
     catch (exception const &e)
     {
