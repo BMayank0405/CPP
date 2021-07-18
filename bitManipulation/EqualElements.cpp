e5ca27dfa61d7d757b1f715168a0840745fab84a
mayank <mayank.barthwal@xeno.in>
2021-07-18 17:34:10 +0530

    equal el

--- bitManipulation/EqualElements.cpp
+++ bitManipulation/EqualElements.cpp
@@ -1,6 +1,6 @@
 /*
  * author: bmayank
- * URL : #{problem_url}
+ * URL : https://www.hackerearth.com/practice/basic-programming/bit-manipulation/basics-of-bit-manipulation/practice-problems/algorithm/equal-elements-2-db70c1ae/
  */
 
 #include <bits/stdc++.h>
